bb6fe36fd8d7d494a540cb57c730e8d21d4ce74e
Tobias <tobiasandersen1234@gmail.com>
2026-09-22 15:08:39 +0200

    Added: -Start position -Go to next room -What room are we win?

--- src/Position.java
+++ src/Position.java
@@ -1,2 +1,20 @@
+import java.util.Random;
+
 public class Position {
+    private Room currentRoom;
+
+    //Start position
+    public Position(Room firstRoom) {
+        this.currentRoom = firstRoom;
+    }
+    //Go to next room
+    public boolean goToNextRoom() {
+        currentRoom = currentRoom.getRoom();
+        return currentRoom != null;
+    }
+    //What room are we in?
+    public int whereIsPlayer() {
+        return currentRoom.getRoomNumber();
+    }
 }
+
