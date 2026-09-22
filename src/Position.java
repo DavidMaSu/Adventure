b3cb36c545080879fd445e7a93fcfbddfcf38ca3
Tobias <tobiasandersen1234@gmail.com>
2026-09-22 16:00:27 +0200

    switch added to movement

--- src/Position.java
+++ src/Position.java
@@ -8,17 +8,24 @@ public class Position {
         this.currentRoom = firstRoom;
     }
     //Go to next room
-    public boolean goToNextRoom(String direction) {
-        Room nextRoom = currentRoom.(direction);
-        if (nextRoom != null){
-            currentRoom = nextRoom;
-            return true;
+    public boolean movement(String direction) {
+        switch (movement(direction)){
+            case "n" -> {
+                Room nextRoom = currentRoom.getName;
+                if (nextRoom != null){
+                    this.currentRoom = currentRoom.north;
+                }
+            }
+            case "s" -> IO.println("You go South");
+            case "e" -> IO.println("You go East");
+            case "w" -> IO.println("You go West");
+            default -> IO.println("Invalid Command");
         }
         return false;
     }
     //What room are we in?
-    public int whereIsPlayer() {
-        return currentRoom.getRoomNumber();
+    public Room whereIsPlayer() {
+        return currentRoom.getName();
     }
 }
 
