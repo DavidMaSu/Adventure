ededc0b813983c21f588f1f1b73aba727e8cfa9f
DavidMaSu <dasuadicani@gmail.com>
2026-09-22 15:16:52 +0200

    Adventure menus completed

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,6 +1,36 @@
 public class Adventure {
 
-    public boolean StartAdventure () {
-return
+    //Initiates Games from Main
+    public void startAdventure () {
+    boolean running = true;
+        while (running){
+
+            //Display current position
+            //PLACEHOLDER
+
+            String command = IO.readln("Input Command: ");
+
+            //Takes input from room and position to change location or quit
+            switch (command){
+                case "n" -> IO.println("You go North");
+                case "s" -> IO.println("You go South");
+                case "e" -> IO.println("You go East");
+                case "w" -> IO.println("You go West");
+                case "Help", "help" -> displayHelpMenu();
+                case "exit" -> running = false;
+                default -> IO.println("Invalid Command");
+            }
+
+        }
+        IO.println("Exiting Program");
+    }
+
+    private void displayHelpMenu (){
+        IO.println("Type n to go North");
+        IO.println("Type s to go South");
+        IO.println("Type e to go East");
+        IO.println("Type w to go West");
+        IO.println("Type Exit to quit");
     }
 }
+
