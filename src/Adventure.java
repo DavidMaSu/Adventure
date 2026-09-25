39f14bb426c01824b613138d4f87b912c2af1f75
SezerC33 <sezer07021@gmail.com>
2026-09-25 11:06:03 +0200

    Made a for loop for the item search

--- src/Adventure.java
+++ src/Adventure.java
@@ -3,12 +3,14 @@ import java.util.ArrayList;
 public class Adventure {
     //Loads classes
     Player dovakin;
-    Item Sword = new Item("Sword","Magic!");
+    Item test;
 
-    public Adventure() {
+    public Adventure(Item test) {
         Map adventureMap = new Map();
         adventureMap.createMap();
         dovakin = new Player(adventureMap.getFirstRoom());
+        this.test = test;
+        dovakin.addItem(test);
 
     }
 
@@ -47,4 +49,31 @@ public class Adventure {
         return dovakin.getCurrentRoom();
     }
 
+    public boolean search(Item item){
+        IO.println("Item in inventory " + dovakin.getInventory());
+        IO.println("Looking for this item " + item);
+        return dovakin.search(item);
+    }
+
+
+
+//    public Item searchItem(Item taskenHunLederEfter) {
+//        Item itemFound = null;
+//        for (dovakin.getInventory() item : dovakin.getInventory()) {
+//            if (dovakin.getInventory() != null && sæde.getTaske().equals(taskenHunLederEfter)) {
+//                itemFound = sæde.getItem();
+//            }
+//        }
+//        return fundetTaske;
+//    }
+//
+//    public int hvilketNrSædeErTaskenFundetPå(Taske taskenHunLederEfter){
+//        int sædeHvorTaskenLigger = -1;
+//        for (int i = 0; i < sæder.size(); i++) {
+//            if (sæder.get(i).getTaske() != null && sæder.get(i).getTaske().equals(taskenHunLederEfter)) {
+//                sædeHvorTaskenLigger = i + 1;
+//            }
+//        }
+//        return sædeHvorTaskenLigger;
+//    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 void main() {
+    Item sword = new Item("sword", "cool", 1);
     IO.println("Hello group");
-    Adventure adventure = new Adventure();
+    Adventure adventure = new Adventure(sword);
     UserInterface UI = new UserInterface(adventure);
 
     UI.move();
